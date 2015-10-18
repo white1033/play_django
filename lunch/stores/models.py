c6dc6b5d8a7eb07fb3e17ddff90a03908c39893c
white1033 <k75715@gmail.com>
2015-10-18 14:48:24 +0800

    refactor views

--- lunch/stores/models.py
+++ lunch/stores/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.urlresolvers import reverse
 
 
 class Store(models.Model):
@@ -8,6 +9,9 @@ class Store(models.Model):
 
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        return reverse('store_detail', kwargs={'pk': self.pk})
 
 
 class MenuItem(models.Model):
